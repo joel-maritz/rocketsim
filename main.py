a9f17d64d0d75f2bdbbee341133a62b60b70b7d8
joel-martini <maritzjoel@gmail.com>
2026-07-13 21:46:28 +1000

    Add delta time, add mission timer, add simspeed multiplier

--- main.py
+++ main.py
@@ -1,41 +1,59 @@
 import time
-class Sim:
+class Simulation:
     def __init__(self):
-        self.fuel = 500
+        self.fuel = 200
         self.fuelDecayRate = 5
 
         self.speed = 0
         self.altitude = 0
 
-        self.thrust = 50
+        # temporary thrust model
         self.gravity = 9.81
+        self.thrust = self.gravity + 5
+        
+        self.weight = 1000
+        self.maxSpeed = 1200 - self.weight
 
-        self.maxSpeed = 200
+        self.time = 0
+        self.active = True
 
-    def update(self):
-        print('\033c')
+    def update(self, dt):
 
         if self.fuel > 0:
-            self.speed += self.thrust
-            self.fuel -= self.fuelDecayRate
+            self.speed += self.thrust * dt
+            self.fuel -= self.fuelDecayRate * dt
 
-        self.speed -= self.gravity
+        self.speed -= self.gravity * dt
 
         if self.speed > self.maxSpeed:
             self.speed = self.maxSpeed
 
-        self.altitude += self.speed
+        self.altitude += self.speed * dt
 
         if self.altitude < 0:
             self.altitude = 0
             self.speed = 0
-
-        print(f'Speed: {int(self.speed)}')
-        print(f'Altitude: {int(self.altitude)}')
-        print(f'Fuel: {int(self.fuel)}')
+        if self.fuel < 0:
+            self.fuel = 0
         
-sim = Sim()
+        if self.altitude == 0 and self.fuel == 0:
+            self.active = False
+
+        if self.active:
+            self.time += dt
+
+def display(sim):
+    print('\033c')
+    print(f'Speed: {int(sim.speed)}')
+    print(f'Altitude: {int(sim.altitude)}')
+    print(f'Fuel: {int(sim.fuel)}')
+    print(f"Time: T+{sim.time:.1f}s")
+        
+sim = Simulation()
+dt = 0.1
+simspeed = 3
 
 while True:
-    sim.update()
-    time.sleep(0.1)
+    sim.update(dt * simspeed)
+    display(sim)
+    time.sleep(dt)
